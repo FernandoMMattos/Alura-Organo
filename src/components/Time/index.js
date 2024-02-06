2476228a004f00be42fa3be165771727caff0a98
Etcloide <etcloide@gmail.com>
2024-02-06 09:48:13 -0300

    full project

---
+++ src/components/Time/index.js
@@ -0,0 +1,28 @@
+import Colaborador from "../Colaborador";
+import "./Time.css";
+
+const Time = (props) => {
+  const corSecundaria = { backgroundColor: props.corSecundaria };
+  const corPrimaria = { borderColor: props.corPrimaria };
+
+  return props.colaboradores.length > 0 ? (
+    <section className="time" style={corSecundaria}>
+      <h3 style={corPrimaria}>{props.nome}</h3>
+      <div className="colaboradores">
+        {props.colaboradores.map((colaborador) => (
+          <Colaborador
+            key={colaborador.nome}
+            nome={colaborador.nome}
+            cargo={colaborador.cargo}
+            imagem={colaborador.imagem}
+            corPrimaria={props.corPrimaria}
+          />
+        ))}
+      </div>
+    </section>
+  ) : (
+    ""
+  );
+};
+
+export default Time;
